c1997b98247e7a70fa9131091454339164af017d
jinorae <ekblld@gmail.com>
2014-11-22 16:40:32 -0800

    Added sprite zipping to bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function zip_files(){
+    cd $1
+    name=`basename $1 .txt`
+    zip -r $name.zip .
+    mv $name.zip ../
+    cd -
+    rm -Rf $1
+}
+
 [ "$1" == "--nodmg" ] && NODMG=true
 [ "$1" == "--dmgonly" ] && DMGONLY=true
 
@@ -19,6 +28,18 @@ macdeployqt.app/Contents/MacOS/macdeployqt Pokemon-Online.app
 mac-deploy-scripts/bundle_mac_app.sh
 mac-deploy-scripts/fix_plugin_linking.py
 fi
+
+cd Pokemon-Online.app/Contents/Resources/db/pokes
+for g in 1G 2G 3G 4G 5G 6G
+do
+    zip_files $g/sprites
+done
+zip_files icons
+zip_files cries
+cd ..
+zip_files items/items
+zip_files items/berries
+cd ../../../..
 
 if [ -z "$NODMG" ]
 then
